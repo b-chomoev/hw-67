2bda1e1ed8bb488c879957b3386171511145e04b
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-11-07 06:12:48 -0600

    Display and Buttons are created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import PinPad from "./containers/PinPad/PinPad.tsx";
 
 const App = () => {
   return (
       <>
-        Hello World
+          <PinPad />
       </>
   );
 };
--- src/containers/PinPad/PinPad.tsx
+++ src/containers/PinPad/PinPad.tsx
@@ -1,8 +1,20 @@
+import './PinPad.css';
 
 const PinPad = () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
     return (
         <>
+            <div className='display'>
+                <input type='password' />
+            </div>
+            <div className='buttons'>
+                {numbers.map((number) => (
+                    <button key={number}>{number}</button>
+                ))}
+                <button>Del</button>
+                <button>E</button>
+            </div>
 
         </>
     );
